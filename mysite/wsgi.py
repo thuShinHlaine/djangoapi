b2f6b76321ad8d28a549160a945fd85fe45847e6
THUXIN <thushin.tsh@gmail.com>
2020-12-19 23:20:07 +0630

    Commit New

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -11,8 +11,9 @@ import os
 import sys
 from django.core.wsgi import get_wsgi_application
 
-path = r'D:\data from other partiton\python\django_test\mysite'
-sys.path.append(path)
+path = r'D:/data from other partiton/python/django_test/mysite'
+if path not in sys.path:
+    sys.path.append(path)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_wsgi_application()
