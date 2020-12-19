90a8b7dbf7b36fb6bec9888dab8cce08f3f7b106
THUXIN <thushin.tsh@gmail.com>
2020-12-20 00:28:16 +0630

    Commit application

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,11 +8,7 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
-import sys
-from pathlib import Path,os
-from django.core.wsgi import get_wsgi_application
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(BASE_DIR)
+from django.core.wsgi import get_wsgi_application
 os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
