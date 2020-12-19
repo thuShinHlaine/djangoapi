84141aeef576557ae8e99d79f6e7b0e4581dcc99
THUXIN <thushin.tsh@gmail.com>
2020-12-20 00:00:14 +0630

    Commit New

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -12,14 +12,7 @@ import sys
 from pathlib import Path,os
 from django.core.wsgi import get_wsgi_application
 
-BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
-path = r'D:\data from other partiton\python\django_test\mysite'
-print(sys.path)
-if path not in sys.path:
-    sys.path.insert(path)
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
-
-application = get_wsgi_application()
-import django
-django.setup()
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
