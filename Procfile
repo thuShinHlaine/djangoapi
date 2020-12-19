95da17f7aa658029cec85b4318ed5a2c016a0ec7
THUXIN <thushin.tsh@gmail.com>
2020-12-19 23:12:57 +0630

    Commit New

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn mysite.wsgi:application --preload --workers 1
+web: gunicorn mysite.wsgi:application --log-file -
--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
+path = 'D:\data from other partiton\python\django_test\mysite'
+sys.path.append(path)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_wsgi_application()
