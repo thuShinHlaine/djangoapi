04836b25d5e74f88578ba6375bdb46a98442e87b
THUXIN <thushin.tsh@gmail.com>
2020-12-20 19:00:09 +0630

    Modified proc

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls import  url
 
 
 urlpatterns = [
+    
+    url(r'^$', 'path.to.function'),
     path('admin/', admin.site.urls),
     path('myapi/', include('myapi.urls')),
 ]
