61d6c621173bbd58b95ce77a80b4fec603680b06
THUXIN <thushin.tsh@gmail.com>
2020-12-20 18:25:33 +0630

    Modified proc

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 
 import os
 import sys
-path='/python/django_test/mysite'
+path='D:/data from other partiton/python/django_test/mysite'
 if path not in sys.path:
     sys.path.append(path)
 
