e7a0df057e649e8180f8643e4db57ee762989bf8
thuxin <thushin.tsh@gmail.com>
2021-01-08 15:17:17 +0630

    secret

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,10 +17,11 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls import  url
 from django.conf import settings
-from django.views.static import serve 
+from django.conf.urls.static import static
+
 urlpatterns = [
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+     
     path('admin/', admin.site.urls),
     path('myapi/', include('myapi.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
