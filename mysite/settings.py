0a615bd74392f5ff57d4cf45e27d30c194cee236
thuxin <thushin.tsh@gmail.com>
2021-01-11 10:17:45 +0630

    root

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = 'ss&+owk*my7xj!r)xtzeqe(x46_s9n8n(*utnsvk0+$$!()#34'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
                  'myheroapi.herokuapp.com/myapi',
